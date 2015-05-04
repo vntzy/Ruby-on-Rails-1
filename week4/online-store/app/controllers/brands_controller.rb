e7524f939f869e67b7fceee807c73de8fbf1adbd
vntzy <vntzyv@gmail.com>
2015-05-04 19:11:18 +0300

    add some controller actions

--- week4/online-store/app/controllers/brands_controller.rb
+++ week4/online-store/app/controllers/brands_controller.rb
@@ -1,4 +1,31 @@
 class BrandsController < ApplicationController
   include Restable
   include Rangable
+
+  def index
+    render json: Brand.all
+  end
+
+  def new
+  end
+
+  def create
+    Brand.create name: params[:name]
+  end
+
+  def show
+    render json: Brand.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    Brand.find params[:id]
+    render json: params
+  end
+
+  def destroy
+
+  end
 end
